957f297131fb32b3e3f4a57ee14092168e42663f
isaacyule <isaac.k.yule@gmail.com>
2017-08-26 17:03:34 +0100

    handlebars view engine

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require("express");
+var expressHandlebars = require("express-handlebars");
+
 var app = express();
+
+app.engine(".handlebars", expressHandlebars({extname: ".handlebars"}));
+
+app.set("view engine", ".handlebars");
 
 var testHandler = function(request, response) {
 	console.log("Hello from Isaac");
-	response.send("hello from testHandler");
+	response.render("home");
 };
 
 app.get("/test", testHandler);
